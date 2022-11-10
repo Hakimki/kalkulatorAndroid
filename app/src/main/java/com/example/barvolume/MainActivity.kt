4fb687a4da2bea900d5bb1d4fee8dc0f53f2dd7c
Hakimki <fahmihhakim8@gmail.com>
2022-11-10 08:00:52 +0700

    hehehe

---
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.barvolume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_witdh)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+    }
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+
+}
